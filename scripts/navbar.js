a49ec692f2d2ce21428a64ee6d128c5b45129c47
Matthew McCaskill <mattrmccaskill@gmail.com>
2018-09-23 16:51:31 -0500

    Make navbar work on mobile

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,11 +2,13 @@ window.onscroll = function() {updateNavbar()};
 
 var navbar = document.getElementsByClassName("navbar-holster")[0];
 var secondaryNavbar = document.getElementsByClassName("navbar-secondary")[0];
+var logoimg = document.getElementById("testimg");
 
 var sticky = navbar.offsetTop;
-var secondaryNavbarEntryPoint = 500;
 
 function updateNavbar() {
+    if (logoimg === null) logoimg = document.getElementById("testimg");
+    let rect = logoimg.getBoundingClientRect();
     if (window.pageYOffset > sticky) {
         navbar.classList.add("sticky")
         document.body.classList.add("sticky-navbar");
@@ -14,7 +16,7 @@ function updateNavbar() {
         navbar.classList.remove("sticky");
         document.body.classList.remove("sticky-navbar");
     }
-    if (window.pageYOffset > secondaryNavbarEntryPoint) {
+    if (window.pageYOffset > rect.x + 0.4 * rect.height) {
         secondaryNavbar.classList.remove("hidden");
         document.body.classList.add("sticky-navbar-secondary");
     } else {
